5e656303f12a069dfc8b6794735dbedaf44e0ff9
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-05-27 18:37:56 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from home.models import order,Product,signup
+
+
+
+
+admin.site.register(Product)
+admin.site.register(signup)
+admin.site.register(order)
+# admin.site.register(Users)
